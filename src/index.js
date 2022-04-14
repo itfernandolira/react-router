9f0034b3a7a6b47724d3cc65f387a56aa110de79
Fernando Lira <it.fernandolira@gmail.com>
2022-04-14 10:30:16 +0100

    Project structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './views/App';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <h1>App Router</h1>
+    <App></App>
 );
 
 
