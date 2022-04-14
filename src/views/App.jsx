7de424e8bbbe1d2225ea660fac3ab263ce39553e
Fernando Lira <it.fernandolira@gmail.com>
2022-04-14 11:03:23 +0100

    Styling

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
 import React from 'react';
+import Menu from '../components/layout/Menu'
+import Content from '../components/layout/Content'
 
 const App = () => (
     <div className='App'>
-        Component App
+        <Menu />
+        <Content />
     </div>
 )
 
